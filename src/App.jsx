c9d17141b4bd29af3c387dfff9c7bfafe2620cb3
jeyarish <jeyarish2003@gmail.com>
2024-02-17 22:13:31 +0530

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,18 @@ import Nav from "./Components/Nav";
 import Home from "./Components/Home";
 import Education from "./Components/Education/Education";
 import Contact from "./Components/Contact";
-import { Link } from "react-scroll";
-import pic from ".//Asset//scroll-up.jpeg";
+
+import Scrollup from "./Scrollup";
+
+// import Scroll from "./Components/Scroll";
 function App() {
   return (
     <div>
-      <Nav />
-      <Home />
-      <Education />
-      <Contact />
-      <Link to="home" offset={-100} smooth={true} duration={500}>
-        <img
-          className=" relative -left-full bottom-3"
-          src={pic}
-          width="50"
-          height="50"
-          alt=""
-        />
-      </Link>
+      <Nav></Nav>
+      <Home></Home>
+      <Education></Education>
+      <Contact></Contact>
+      <Scrollup />
     </div>
   );
 }
